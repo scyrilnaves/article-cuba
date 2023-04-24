855e71806c0c3fc8d9d9cdd91b7aff85ff7bb7fd
cyril naves <cyril-naves.samuel@renault.com>
2023-04-24 15:19:33 +0200

    changes

--- cubaclouddeploy/deploy/login.sh
+++ cubaclouddeploy/deploy/login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yes | ./rancher login https://rancher.unice.cust.tasfrance.com/v3 --token token-xnkqf:l99wblgctvwhcrbzpspnhjbx2ppxfkhwppg8g8nkct9lzhpnlgzh8b
+yes | ./rancher login https://rancher.unice.cust.tasfrance.com/v3 --token token-xnkqf
